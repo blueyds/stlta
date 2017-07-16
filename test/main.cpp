874d0013f3b38c8033b7d8169e36a4eea96af1f4
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-07-15 23:13:10 -0500

    removed c11 dependency

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include "../stlta/functions.h"
 
+void printVector(TA::dvector v)
+{
+	std::copy(v.begin(), v.end(), std::ostream_iterator<double>(std::cout, "\t"));
+}
+
 
 int main (int argc, char *argv[])
 {
@@ -12,10 +17,10 @@ int main (int argc, char *argv[])
 	
 	dvector closes = {10,11,12,13,14,15,14.5,13,12,10,9,8,7,8,9,11,13,14,16,18,20,18,16,15,14,12,10,14,16,19,20.1};
 	cout << "using the following vector to test\n";
-	for (auto i = closes.begin(); i != closes.end(); ++i) std::cout << *i << "\t";
+	printVector(closes);
 	cout << "\n\nTesting SMA function : \t";
 	dvector SMAs10;
 	SMA(10,closes,SMAs10);
-	for (auto i = SMAs10.begin(); i != SMAs10.end(); ++i) std::cout << *i << "\t";
+	printVector(SMAs10);
 	cout << "\n\nFinished testing STL_TA\n";
 }
