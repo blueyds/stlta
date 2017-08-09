3a47d9f768a63b63415f19353cf1592b5c8abfc1
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-08-09 14:02:15 -0500

    removed namespace using lines

--- test/main.cpp
+++ test/main.cpp
@@ -7,12 +7,10 @@
 
 BOOST_AUTO_TEST_CASE(TA_test)
 {
-	using std;
-	using TA;
-	dvector closes = {10,11,12,13,14,15,14.5,13,12,10,9,8,7,8,9,11,13,14,16,18,20,18,16,15,14,12,10,14,16,19,20.1};
+	TA::dvector closes = {10,11,12,13,14,15,14.5,13,12,10,9,8,7,8,9,11,13,14,16,18,20,18,16,15,14,12,10,14,16,19,20.1};
 	dvector SMAs10;
 	dvector hSMAs10;
-	SMA(10,closes,SMAs10);
-	xSMA(10,closes,hSMAs10);
+	TA::SMA(10,closes,SMAs10);
+	TA::xSMA(10,closes,hSMAs10);
 	BOOST_CHECK(SMAs10==hSMAs10);
 };
