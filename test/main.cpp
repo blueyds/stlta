0feb2d29ec9e8b27168946adbc3fd85345c475ee
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-08-09 14:45:34 -0500

    vector initialiation

--- test/main.cpp
+++ test/main.cpp
@@ -7,7 +7,43 @@
 
 BOOST_AUTO_TEST_CASE(TA_test)
 {
-	TA::dvector closes[31] = {10.0,11.0,12.0,13.0,14.0,15.0,14.5,13.0,12.0,10.0,9.0,8.0,7.0,8.0,9.0,11.0,13.0,14.0,16.0,18.0,20.0,18.0,16.0,15.0,14.0,12.0,10.0,14.0,16.0,19.0,20.1};
+	TA::dvector closes; 
+	
+	closes.push_back(10.0);
+	closes.push_back(11.0);
+	closes.push_back(12.0);
+	closes.push_back(13.0);
+	closes.push_back(14.0);
+	closes.push_back(15.0);
+	closes.push_back(14.5);
+	closes.push_back(13.0);
+	closes.push_back(12.0);
+	closes.push_back(10.0);
+	closes.push_back(9.0);
+	closes.push_back(8.0);
+	closes.push_back(7.0);
+	closes.push_back(8.0);
+	closes.push_back(9.0);
+	closes.push_back(11.0);
+	closes.push_back(13.0);
+	closes.push_back(14.0);
+	closes.push_back(16.0);
+	closes.push_back(18.0);
+	closes.push_back(20.0);
+	closes.push_back(18.0);
+	closes.push_back(16.0);
+	closes.push_back(15.0);
+	closes.push_back(14.0);
+	closes.push_back(12.0);
+	closes.push_back(10.0);
+	closes.push_back(14.0);
+	closes.push_back(16.0);
+	closes.push_back(19.0);
+	closes.push_back(20.0);
+	closes.push_back(20.5);
+	closes.push_back(18.0);
+	closes.push_back(19.0);
+	
 	TA::dvector SMAs10;
 	TA::dvector hSMAs10;
 	TA::SMA(10,closes,SMAs10);
