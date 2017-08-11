abe7b9c59bc92e3fb197f32c1ff960484f790096
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-08-11 18:23:14 -0500

    implemented a more complete stlta.hpp

--- stlta/stlta.hpp
+++ stlta/stlta.hpp
@@ -21,16 +21,9 @@
 
 #include <vector>
 #include <algorithm>
-#include "stlta/functions.h"
-#include "stlta/sma.h"
-#include "stlta/ema.h"
-#include "stlta/subtract.h"
-#include "stlta/max.h"
-#include "stlta/min.h"
-#include "stlta/momentum.h"
-#include "stlta/roc.h"
+#include <numeric>
 
-namespace TA
+namespace hTA
 {
 //	typedef std::vector<double> dvector;
 /*
@@ -44,13 +37,281 @@ namespace TA
 	void MOMENTUM(int window,dvector &input,dvector &output);
 	void ROC(int window,dvector &input,dvector &output);
 */  
+namespace functors{
+template <typename T>
+class SMA 
+{
+	std::vector<T> window_values;
+	int window;
+	int curSize;
+public:
+	SMA(int win) //window is the size of SMA lookback
+	{
+		window=win;
+		curSize=0;
+		window_values.reserve(window+1);
+	};
+	T operator()(T& value)
+	{
+		window_values.push_back(value);
+		curSize = curSize +1;
+		T val = 0;
+		if (curSize == window) 
+		{
+			curSize=curSize -1;
+			//T new_value;
+			T sum_value;
+			sum_value = std::accumulate(window_values.begin(),window_values.end(),0);
+			val= sum_value / window;
+			window_values.erase(window_values.begin());
+		}
+//		std::cout << "return curSize = " <<curSize << "\t window ="<< window << "\tclosing = "<< value << "\tSMA ="<< val <<"\n";
+		return val;
+	}
+	
+	
+};
+
+template <class T>
+class SUBTRACT
+{
+public:
+	SUBTRACT () {;};
+	T operator()( T a, T b ) const    
+    {    
+        return a - b;    
+    }  
+};
+
+template <typename T>
+class ROC 
+{
+	std::vector<T> window_values;
+	int window;
+	int curSize;
+public:
+	ROC(int win) //window is the size of SMA lookback
+	{
+		window=win;
+		curSize=0;
+		window_values.reserve(window+1);
+	};
+	T operator()(T& value)
+	{
+		window_values.push_back(value);
+		curSize = curSize +1;
+		T val = 0;
+		if (curSize == window) 
+		{
+			curSize=curSize -1;
+			val= value / *(window_values.begin());
+			window_values.erase(window_values.begin());
+		}
+//		std::cout << "return curSize = " <<curSize << "\t window ="<< window << "\tclosing = "<< value << "\tSMA ="<< val <<"\n";
+		return val;
+	}
+};
+
+
+template <typename T>
+class MOMENTUM 
+{
+	std::vector<T> window_values;
+	int window;
+	int curSize;
+public:
+	MOMENTUM(int win) //window is the size of SMA lookback
+	{
+		window=win;
+		curSize=0;
+		window_values.reserve(window+1);
+	};
+	T operator()(T& value)
+	{
+		window_values.push_back(value);
+		curSize = curSize +1;
+		T val = 0;
+		if (curSize == window) 
+		{
+			curSize=curSize -1;
+			val= value - *(window_values.begin());
+			window_values.erase(window_values.begin());
+		}
+//		std::cout << "return curSize = " <<curSize << "\t window ="<< window << "\tclosing = "<< value << "\tSMA ="<< val <<"\n";
+		return val;
+	}
+};
+template <typename T>
+class MIN 
+{
+	std::vector<T> window_values;
+	int window;
+	int curSize;
+public:
+	MIN(int win) //window is the size of lookback 
+	{
+		window=win;
+		curSize=0;
+		window_values.reserve(window+1);
+	};
+	T operator()(T& value)
+	{
+		window_values.push_back(value);
+		curSize++;
+		T val = 0;
+		if (curSize == window) 
+		{
+			curSize--;
+			//T new_value;
+			val = *(std::min_element( window_values.begin(), window_values.end() ) );
+			window_values.erase(window_values.begin());
+		}
+//		std::cout << "return curSize = " <<curSize << "\t window ="<< window << "\tclosing = "<< value << "\tSMA ="<< val <<"\n";
+		return val;
+	}
+};
+
+template <typename T>
+class MAX 
+{
+	std::vector<T> window_values;
+	int window;
+	int curSize;
+public:
+	MAX(int win) //window is the size of lookback 
+	{
+		window=win;
+		curSize=0;
+		window_values.reserve(window+1);
+	};
+	T operator()(T& value)
+	{
+		window_values.push_back(value);
+		curSize = curSize +1;
+		T val = 0;
+		if (curSize == window) 
+		{
+			curSize=curSize -1;
+			//T new_value;
+			val = *( std::max_element( window_values.begin(), window_values.end()) );
+			window_values.erase(window_values.begin());
+		}
+//		std::cout << "return curSize = " <<curSize << "\t window ="<< window << "\tclosing = "<< value << "\tSMA ="<< val <<"\n";
+		return val;
+	}
+};
+template <class T>
+class EMA
+{
+	std::vector<T> window_values;
+	int window;
+	T K;
+	int counter;
+	T prior_ema;
+public:
+	EMA(int win) //window is the size of EMA lookback
+	{
+		window=win;
+		counter=0;
+		prior_ema = 0;
+		K = 2 / (window+1);
+		window_values.reserve(window+1);
+	};
+	T operator()(T& value)
+	{
+		window_values.push_back(value);
+		counter = counter +1;
+		T val = 0;
+		if (counter==window)  
+		{	
+			if (prior_ema==0)
+			{
+				T sum_value;
+				val = std::accumulate( window_values.begin(), window_values.end(), 0) / window;
+				prior_ema=val;
+				window_values.erase(window_values.begin());
+			}
+			else
+			{
+				T closingK = value * K;
+				T priorK = prior_ema * (1-K);
+				val = closingK + prior_ema;
+				prior_ema = val;
+			}
+		}
+		return val;
+	}
+};
+
+}//namespace TA::functors
+
 // header based definitions
-	inline void xSMA(int window, dvector &input, dvector &output)
-    {
-    	TA::functors::SMA<double> _sma(window);
-		output.reserve(input.size());
-		std::transform(input.begin(), input.end(), output.begin(), _sma);
-    }
+inline void SMA(int window, dvector &input, dvector &output)
+{
+	functors::SMA<double> _sma(window);
+	output.reserve(input.size());
+	std::transform(input.begin(), input.end(), output.begin(), _sma);
+};
+
+inline void EMA(int window,dvector &input, dvector &output)
+{
+	functors::EMA<double> _ema(window);
+	output.reserve(input.size());
+	std::transform( input.begin(), input.end(), output.begin(), _ema);
+}
+
+inline void SUBTRACT(dvector &input1, dvector &input2, dvector &output)
+{//1 - 2
+	functors::SUBTRACT<double> _subtract;
+	output.reserve(input1.size());
+	std::transform(input1.begin(), input1.end(),input2.begin(),output.begin(), _subtract);
+}
+
+inline void MIN(int window,dvector &input, dvector &output)
+{
+	functors::MIN<double> _min(window);
+	output.reserve(input.size());
+	std::transform( input.begin(), input.end(), output.begin(), _min);
+}
+
+inline void MAX(int window,dvector &input, dvector &output)
+{
+	functors::MAX<double> _max(window);
+	output.reserve(input.size());
+	std::transform( input.begin(), input.end(), output.begin(), _max);
+}
+
+inline void MOMENTUM(int window,dvector &input,dvector &output)
+{
+	functors::MOMENTUM<double> _momentum(window);
+	output.reserve(input.size());
+	std::transform( input.begin(), input.end(), output.begin(), _momentum);
+}
+
+inline void ROC(int window,dvector &input,dvector &output)
+{
+	functors::ROC<double> _roc(window);
+	output.reserve(input.size());
+	std::transform( input.begin(), input.end(), output.begin(), _roc);
+}
+
+
+inline void MACD(int short_window, int long_window, int smooth_window, dvector &input, dvector &macd, dvector &macd_signal, dvector &macd_hist)
+{
+	std::vector<double> short_emas;
+	std::vector<double> long_emas;
+	short_emas.reserve(input.size());
+	long_emas.reserve(input.size());
+	macd.reserve(input.size());
+	macd_signal.reserve(input.size());
+	macd_hist.reserve(input.size());
+	EMA(short_window,input,short_emas);
+	EMA(long_window,input,long_emas);
+	SUBTRACT(short_emas,long_emas,macd);
+	EMA(smooth_window,macd,macd_signal);
+	SUBTRACT(macd,macd_signal,macd_hist);
+}
+
 }//namespace TA
 
 
--- test/main.cpp
+++ test/main.cpp
@@ -43,10 +43,52 @@ BOOST_AUTO_TEST_CASE(TA_test)
 	closes.push_back(20.5);
 	closes.push_back(18.0);
 	closes.push_back(19.0);
-	
+	{
 	TA::dvector SMAs10;
-	TA::dvector hSMAs10;
+	hTA::dvector hSMAs10;
 	TA::SMA(10,closes,SMAs10);
-	TA::xSMA(10,closes,hSMAs10);
+	hTA::SMA(10,closes,hSMAs10);
 	BOOST_CHECK(SMAs10==hSMAs10);
+	}
+	{
+	TA::dvector EMAs10;
+	hTA::dvector hEMAs10;
+	TA::EMA(10,closes,EMAs10);
+	hTA::EMA(10,closes,hEMAs10);
+	BOOST_CHECK(EMAs10==hEMAs10);
+	}
+	{
+	TA::dvector MINs10;
+	hTA::dvector hMINs10;
+	TA::MIN(10,closes,MINs10);
+	hTA::MIN(10,closes,hMINs10);
+	BOOST_CHECK(MINs10==hMINs10);
+	}
+	{
+	TA::dvector MAXs10;
+	hTA::dvector hMAXs10;
+	TA::MAX(10,closes,MAXs10);
+	hTA::MAX(10,closes,hMAXs10);
+	BOOST_CHECK(MAXs10==hMAXs10);
+	}
+	{
+	TA::dvector ROCs10;
+	hTA::dvector hROCs10;
+	TA::ROC(10,closes,ROCs10);
+	hTA::ROC(10,closes,hROCs10);
+	BOOST_CHECK(ROCs10==hROCs10);
+	}
+	{
+	TA::dvector MACDs;
+	TA::dvector signals;
+	TA::dvector hists;
+	hTA::dvector hMACDs;
+	hTA::dvector hsignals;
+	hTA::dvector hhists;
+	TA::MACD(12,26,9,closes,MACDs,signals,hists);
+	hTA::MACD(12,26,9,closes,hMACDs,hsignals,hhists);
+	BOOST_CHECK(MACDs==hMACDs);
+	BOOST_CHECK(signals==hsignals);
+	BOOST_CHECK(hists==hhists);
+	}
 };
