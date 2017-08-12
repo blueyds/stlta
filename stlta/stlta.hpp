8975142dd015ddd0a4a92d674e934209fcf3869d
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-08-11 20:23:50 -0500

    dvector typedef

--- stlta/stlta.hpp
+++ stlta/stlta.hpp
@@ -25,18 +25,8 @@
 
 namespace hTA
 {
-//	typedef std::vector<double> dvector;
-/*
-// non header based definitions. impletemtned in functions.cpp
-	void SMA(int window,dvector &input, dvector &output);
-	void EMA(int window,dvector &input, dvector &output);
-	void SUBTRACT(dvector &input1, dvector &input2, dvector &output);
-	void MACD(int short_window, int long_window, int smooth_window, dvector &input, dvector &macd, dvector &macd_signal, dvector &macd_hist);
-	void MIN(int window, dvector &input, dvector &output);
-	void MAX(int window,dvector &input, dvector &output);
-	void MOMENTUM(int window,dvector &input,dvector &output);
-	void ROC(int window,dvector &input,dvector &output);
-*/  
+typedef std::vector<double> dvector;
+ 
 namespace functors{
 template <typename T>
 class SMA 
